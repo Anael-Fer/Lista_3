5abf2f83d0d2bdec8521a3a65ac9b11ad3b72a2a
Anael-Fer <166033786+Anael-Fer@users.noreply.github.com>
2024-04-17 06:20:14 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace Lista_03
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, i;          
+            
+            Console.WriteLine("Informe um número impar");
+            x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe outro número impar");
+            y = int.Parse(Console.ReadLine());
+
+            i = x;
+
+            Console.WriteLine("A sequência de números são: ");
+
+            while (i <= y)
+            {
+                  
+                Console.WriteLine(i.ToString());
+                i += 2;
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
